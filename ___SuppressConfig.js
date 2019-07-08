eccd21ed7f1428cb327ac4c35619838d7816bb4b
johnvidar <52234531+johnvidar@users.noreply.github.com>
2019-07-08 14:42:18 +0200

    Update ___SuppressConfig.js

--- ___SuppressConfig.js
+++ ___SuppressConfig.js
@@ -8,7 +8,7 @@ class SuppressConfig {
 
     static var TableSuppressValue = 5;        // Min number of answers in the Aggregated Table
     static var VerbatimSuppressValue = 5;     // Min number of answers in the Verbatim Table
-    static var CommentSuppressValue = 5;      // Min number of answers in the Hitlist
+    static var CommentSuppressValue = 20;      // Min number of answers in the Hitlist
     static var CategoricalSuppressValue = 5; // Min number of answers for cards on the Categorical page
 
     // minGap: min difference between neighbour units
