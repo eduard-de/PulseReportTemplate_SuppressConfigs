5d7427afd0e091f2e447aa413662571473ccb2a7
EkaterinaT-CIT <EkaterinaT-CIT@users.noreply.github.com>
2020-01-23 15:38:43 +0300

    Update ___SuppressConfig.js
    add BenchmarkSuppressValue

--- ___SuppressConfig.js
+++ ___SuppressConfig.js
@@ -10,6 +10,7 @@ class SuppressConfig {
     static var VerbatimSuppressValue = 1;     // Min number of answers in the Verbatim Table
     static var CommentSuppressValue = 1;      // Min number of answers in the Hitlist
     static var CategoricalSuppressValue = 1; // Min number of answers for cards on the Categorical page
+    static var BenchamrkSuppressValue = 5; // Min number of answers for benchmark value to be shown in Results table
 
     // minGap: min difference between neighbour units
     // unitSufficientBase: min number of the responses when a unit is always shown irrespective of <minGap> requirement
