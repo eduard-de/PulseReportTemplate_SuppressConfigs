aa463738fae22474d6ac7bcf5f07ffb35cc296b8
ElenaM717 <58686230+ElenaM717@users.noreply.github.com>
2020-02-05 12:01:10 +0300

    Update ___SuppressConfig.js

--- ___SuppressConfig.js
+++ ___SuppressConfig.js
@@ -4,13 +4,13 @@ class SuppressConfig {
     // DATA SUPPRESSION (more info: SuppressUtil)
     //================================================================================
 
-    static var ReportBaseSuppressValue = 5;  // Min number of responses (response = question specified below in Survey Config -> Response: {qId: 'status', codes: ['complete']})
+    static var ReportBaseSuppressValue = 1;  // Min number of responses (response = question specified below in Survey Config -> Response: {qId: 'status', codes: ['complete']})
 
-    static var TableSuppressValue = 1;        // Min number of answers in the Aggregated Table
+    static var TableSuppressValue = 5;        // Min number of answers in the Aggregated Table
     static var VerbatimSuppressValue = 1;     // Min number of answers in the Verbatim Table
     static var CommentSuppressValue = 1;      // Min number of answers in the Hitlist
     static var CategoricalSuppressValue = 1; // Min number of answers for cards on the Categorical page
-    static var BenchamrkSuppressValue = 5; // Min number of answers for benchmark value to be shown in Results table
+    static var BenchamrkSuppressValue = 3; // Min number of answers for benchmark value to be shown in Results table
 
     // minGap: min difference between neighbour units
     // unitSufficientBase: min number of the responses when a unit is always shown irrespective of <minGap> requirement
